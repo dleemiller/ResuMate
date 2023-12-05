8e47cecfd4972bb5866a2af3dd2fed5eaedc41e5
Lee Miller <dleemiller@protonmail.com>
2023-12-05 16:06:24 -0700

    ends sleep faster so it doesn't take up to 10s to exit

--- tasks/dot_logger.py
+++ tasks/dot_logger.py
@@ -2,16 +2,22 @@ import logging
 import threading
 import time
 
+
 class DotLogger:
-    def __init__(self, logger: logging.Logger, timeout:int=10):
+    def __init__(self, logger: logging.Logger, timeout: int = 10):
         self.logger = logger
         self.stop_event = threading.Event()
         self.timeout = timeout
 
-    def log_dot(self):
+    def log_dot(self, interval: float = 0.2):
+        counter = 0
         while not self.stop_event.is_set():
-            time.sleep(self.timeout)
-            self.logger.info(f"Running... {time.time() - self.start_time:.1f}s")
+            time.sleep(interval)
+            if counter >= self.timeout:
+                self.logger.info(f"Running... {time.time() - self.start_time:.1f}s")
+                counter = 0
+            else:
+                counter += interval
 
     def start_logging(self):
         self.thread = threading.Thread(target=self.log_dot)
